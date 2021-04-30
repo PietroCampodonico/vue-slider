e2d4157578d0b5ef9af2b0daf859f694ac4f4772
PietroCampodonico <pietro.campodonico@outlook.it>
2021-04-30 17:10:12 +0200

    aggiunta cartella img

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-// var app = new Vue(
-//     {
-//         el: "#caroselContainer",
-//         data: {
-            
-//         },
-
-//         methods: {
-//         }
-//     })
+const app = new Vue ({
+    
+    el: "#root",
+    data: {
+        images: [{
+            img1: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fs.sbito.it%2Fimages%2F21%2F210a4f48-3e12-4949-a02b-3f5f14624ed3.jpg&imgrefurl=https%3A%2F%2Fwww.subito.it%2Fanimali%2Fmonta-shiba-inu-3-anni-bologna-377696229.htm&tbnid=JmBriRcSvePmGM&vet=12ahUKEwjJ4Y75nqbwAhVOy7sIHV2fALkQMygEegUIARDRAQ..i&docid=W_8PY3ykafhAwM&w=351&h=468&q=shiba%20inu&client=opera-gx&ved=2ahUKEwjJ4Y75nqbwAhVOy7sIHV2fALkQMygEegUIARDRAQ",
+            img2: "",
+            img3: "",
+            img4: "",
+            img5: "",
+        }],
+        activeImage: 0,
+    }
+})
